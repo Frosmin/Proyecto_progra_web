182d0068952de531aa5ef14d33424b8d3d34c749
Frosmin <simonabastomart@gmail.com>
2025-05-07 00:15:51 -0400

    feat: agregar redirección a la ruta raíz y hacer el título clickeable en el navbar

--- frontend/angular/src/app/app.routes.ts
+++ frontend/angular/src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { FormComponent } from './form/form.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' },
+
   { path: 'crear', component: FormComponent}
 ];
