772e850903f5eb7e78bdc758e25c91697e1935eb
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-05-07 00:45:21 -0400

    add: router to view form

--- frontend/angular/src/app/app.routes.ts
+++ frontend/angular/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { FormComponent } from './form/form.component';
+import { ViewFormComponent } from './viewForm/view.form.component';
 
 export const routes: Routes = [
-
+  { path: 'viewform', component: ViewFormComponent},
   { path: 'crear', component: FormComponent}
 ];
