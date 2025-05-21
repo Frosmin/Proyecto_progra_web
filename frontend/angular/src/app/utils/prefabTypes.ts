ea826f27701689f4759f88a7fd65ee542d1af2af
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-05-21 00:40:26 -0400

    fix: imports de prefabTypes

--- frontend/angular/src/app/utils/prefabTypes.ts
+++ frontend/angular/src/app/utils/prefabTypes.ts
@@ -1,38 +1,40 @@
-import { CheckBoxQuestionComponent, MultipleChoiceQuestionComponent, TextQuestionComponent } from "../Prefabs/question-types.component";
+import { CheckboxComponent } from "../Prefabs/checkbox/checkbox.component";
+import { MultipleChoiceComponent } from "../Prefabs/multiple-choice/multiple-choice.component";
+import { TextQuestionComponent } from "../Prefabs/text-question/text-question.component";
 
 export enum PrefabTypes {
-    Basic = 'Basic',
-    Question = 'Question',
+  Basic = 'Basic',
+  Question = 'Question',
 }
 
 export enum QuestionTypes {
-    Text = 'Text',
-    MultipleChoice = 'MultipleChoice',
-    CheckBox = 'CheckBox',
+  Text = 'Text',
+  MultipleChoice = 'MultipleChoice',
+  CheckBox = 'CheckBox',
 }
 
 export const prefabOptions = [
-    {
-        title: 'Útiles',
-        type: PrefabTypes.Basic,
-        prefabs: []
-    },
-    {
-        title: 'Preguntas',
-        type: PrefabTypes.Question,
-        prefabs: [
-            {
-                name: 'Texto',
-                type: TextQuestionComponent,
-            },
-            {
-                name: 'Opción Múltiple',
-                type: MultipleChoiceQuestionComponent,
-            },
-            {
-                name: 'CheckBox',
-                type: CheckBoxQuestionComponent,
-            },
-        ],
-    },
+  {
+    title: 'Útiles',
+    type: PrefabTypes.Basic,
+    prefabs: [],
+  },
+  {
+    title: 'Preguntas',
+    type: PrefabTypes.Question,
+    prefabs: [
+      {
+        name: 'Texto',
+        type: TextQuestionComponent,
+      },
+      {
+        name: 'Opción Múltiple',
+        type: MultipleChoiceComponent,
+      },
+      {
+        name: 'CheckBox',
+        type: CheckboxComponent,
+      },
+    ],
+  },
 ];
