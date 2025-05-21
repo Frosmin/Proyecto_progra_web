acb69b4417da0f0e1301f5177e2eb6da59b157b1
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-05-20 23:56:18 -0400

    refactor: template_editor a su respectivo file html

--- frontend/angular/src/app/Components/component.template_editor.ts
+++ frontend/angular/src/app/Components/component.template_editor.ts
@@ -5,18 +5,8 @@ import { PrefabComponent } from '../Prefabs/prefab.component';
 
 @Component({
     selector: 'app-template-editor',
-    template: `
-        <div>
-            <h2>Prefab List</h2>
-            <ng-container *ngFor="let prefab of prefabs">
-                <ng-container *ngComponentOutlet="prefab"></ng-container>
-            </ng-container>
-            <ng-template #noPrefabsMessage>
-                <p>No prefabs available.</p>
-            </ng-template>
-        </div>
-    `,
     imports: [CommonModule],
+    templateUrl: './component.template_editor.html',
     styleUrls: ['./component.template_editor.css'],
 })
 export class TemplateEditorComponent {
