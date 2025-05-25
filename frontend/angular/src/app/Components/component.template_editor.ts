9227f0480d5606353145eb6407baaa2b86c60624
Frosmin <simonabastomart@gmail.com>
2025-05-25 00:29:04 -0400

    add: implementar tablero dinámico y estilos para el formulario

--- frontend/angular/src/app/Components/component.template_editor.ts
+++ frontend/angular/src/app/Components/component.template_editor.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Component, Type } from '@angular/core';
 import { PrefabComponent } from '../Prefabs/prefab.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-template-editor',
-    imports: [CommonModule],
+    imports: [CommonModule, FormsModule],
     templateUrl: './component.template_editor.html',
     styleUrls: ['./component.template_editor.css'],
+    standalone: true
 })
 export class TemplateEditorComponent {
     prefabs: Type<PrefabComponent>[] = [];
+    
+    tableroSize: number = 0;
+    tablero: number[][] = [];
+    tableroCreado: boolean = false;
 }
