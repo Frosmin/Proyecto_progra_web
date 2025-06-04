01d9cc444eda60b0dca7a1b74608227922421204
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-06-04 02:33:11 -0400

    add: layout

--- frontend/angular/src/app/app.component.ts
+++ frontend/angular/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive, Router } from '@angular/router';
+import {
+  RouterOutlet,
+  RouterLink,
+  RouterLinkActive,
+  Router,
+} from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
@@ -7,12 +12,17 @@ import { FooterComponent } from './shared/footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, CommonModule, NavbarComponent,FooterComponent],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    NavbarComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'FormBuilder';
-  
+
   constructor(public router: Router) {}
 }
--- frontend/angular/src/app/app.routes.ts
+++ frontend/angular/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { FormComponent } from './form/form.component';
 import { ViewFormComponent } from './viewForm/view.form.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: 'viewform', component: ViewFormComponent},
-  { path: 'crear', component: FormComponent}
+  { path: '', component: HomeComponent },
+  { path: 'viewform', component: ViewFormComponent },
+  { path: 'crear', component: FormComponent },
 ];
