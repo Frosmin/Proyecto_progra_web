c8b3d65054118e86f8c98f69a5e48a21c33cacd6
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-06-04 02:32:48 -0400

    edit: navbar, footer component

--- frontend/angular/src/app/shared/navbar/navbar.component.ts
+++ frontend/angular/src/app/shared/navbar/navbar.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+  ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
-}
+export class NavbarComponent {}
