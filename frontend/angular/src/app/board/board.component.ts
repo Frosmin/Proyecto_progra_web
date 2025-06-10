341c3bc772b06dcfa1c6b98f3d7b043af0d087bf
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-06-10 12:56:05 -0400

    delete: missing imports

--- frontend/angular/src/app/board/board.component.ts
+++ frontend/angular/src/app/board/board.component.ts
@@ -1,7 +1,6 @@
 import { EventEmitter, inject, Input, NgModule, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Component, Type } from '@angular/core';
-import { PrefabComponent } from '../Prefabs/prefab.component';
 import { FormsModule } from '@angular/forms';
 import { BoxComponent } from '../Components/box/box.component';
 import {
@@ -32,6 +31,9 @@ export class BoardComponent {
     this._editor = value;
     this.onEditorChange();
   }
+
+  
+
   boardService = inject(BoardService);
   piecePositions: CoordinateDictionary<PiecePosition> = {};
   tableroSize: number = 8;
