45043ef11bca4e94749542c793689307d6a795f9
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-06-10 21:42:54 -0400

    add: boton para agregar tableros

--- frontend/angular/src/app/form/form.component.ts
+++ frontend/angular/src/app/form/form.component.ts
@@ -1,19 +1,33 @@
 import { Component, ViewChild, Type } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BoardComponent } from '../board/board.component';
-import { boardType,boardsMockups } from '../utils/FormType';
+import { boardType, boardsMockups } from '../utils/FormType';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-form',
   // standalone: true,
-  imports: [CommonModule,BoardComponent],
+  imports: [CommonModule, BoardComponent, MatIconModule, MatDividerModule, MatButtonModule],
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent {
-
-  boards : boardType[] = boardsMockups
-
+  boards: boardType[] = [];
+  cnt: number = 1;
   editor: boolean = true; // Para determinar si se está en modo edición o no
 
+  agregarTablero() {
+    const nuevoTablero: boardType = {
+      id: this.cnt.toString(),
+      size: 4,
+      pieces: [],
+    };
+    this.boards.push(nuevoTablero);
+    this.cnt++;
+  }
+  eliminarTablero(index: number) {
+    this.boards.splice(index, 1);
+  }
 }
