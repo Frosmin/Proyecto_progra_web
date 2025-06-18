a8f85856fbee7df1b35f7f4b1ce6b019802014f3
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-06-18 01:56:08 -0400

    add: ruta para la vista de tableros

--- frontend/angular/src/app/app.routes.ts
+++ frontend/angular/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { FormComponent } from './form/form.component';
 import { ViewFormComponent } from './viewForm/view.form.component';
 import { HomeComponent } from './home/home.component';
+import { VistaComponent } from './vista/vista.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'viewform', component: ViewFormComponent },
+  { path: 'view/:id', component: VistaComponent },
   { path: 'crear', component: FormComponent },
 ];
