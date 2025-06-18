bc837290c2d75f2f4b1595283f24bc1dc0c31eed
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-06-18 01:57:34 -0400

    add: llamadas al back

--- frontend/angular/src/app/services/api.service.ts
+++ frontend/angular/src/app/services/api.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { Tablero } from '../utils/TableroTypes';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-  
-  constructor(private http: HttpClient) { }
-  
-  getHelloMessage(): Observable<string> {
-    return this.http.get('/api/hello', { responseType: 'text' });
+  constructor(private http: HttpClient) {}
+
+  getTableros(): Observable<Tablero[]> {
+    return this.http.get<Tablero[]>('http://localhost:8080/api/tableros');
+  }
+  getTablero(id: number): Observable<Tablero> {
+    return this.http.get<Tablero>(`http://localhost:8080/api/tablero/${id}`);
   }
 }
