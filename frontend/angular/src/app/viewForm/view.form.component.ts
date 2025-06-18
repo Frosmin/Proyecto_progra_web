24751cc65cb42171e1b28ad9548b75d738851596
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-06-18 01:58:10 -0400

    update: view form component

--- frontend/angular/src/app/viewForm/view.form.component.ts
+++ frontend/angular/src/app/viewForm/view.form.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
+import { ListComponent } from '../Components/list/list.component';
+import { Tablero } from '../utils/TableroTypes';
 
 @Component({
   selector: 'app-viewform',
-  imports: [],
+  imports: [ListComponent],
   templateUrl: './view.form.component.html',
-  styleUrl: './view.form.component.scss'
+  styleUrl: './view.form.component.scss',
 })
-export class ViewFormComponent {
+export class ViewFormComponent implements OnInit {
+  data: Tablero[] = [];
+  constructor(private apiService: ApiService) {}
 
+  ngOnInit(): void {
+    this.apiService.getTableros().subscribe({
+      next: (data) => {
+        this.data = data;
+        console.log('Mensaje recibido:', this.data);
+      },
+      error: (err) => {
+        console.error('Error al llamar al API:', err);
+      },
+    });
+  }
 }
