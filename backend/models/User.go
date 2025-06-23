42802f4487fe6e67e87366d62ecac5bd4746967a
Frosmin <simonabastomart@gmail.com>
2025-06-23 12:39:36 -0400

    add: implementar cifrado de contraseña y manejo de errores en el registro y inicio de sesión de usuarios

--- backend/models/User.go
+++ backend/models/User.go
@@ -1,6 +1,9 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
@@ -11,4 +14,17 @@ type User struct {
 	Email     string `grorm:"unique;not null"`
 	Password  string
 	Tablero   []Tablero `gorm:"foreignKey:UserID"`
+}
+
+func (u *User) HashPassword() error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashedPassword)
+	return nil
+}
+
+func (u *User) CheckPassword(password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
 }
