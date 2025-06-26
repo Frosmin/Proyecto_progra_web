feac0a00dca7ec2d21219e3595a5bbbe807e5999
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-06-26 14:07:02 -0400

    fix: dockerfiles

--- backend/routes/cors.go
+++ backend/routes/cors.go
@@ -11,7 +11,7 @@ func SetupRouter() *gin.Engine {
 
 	// Configuración de CORS
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://localhost:4200"} // Tu aplicación Angular
+	config.AllowOrigins = []string{"*"} // Tu aplicación Angular
 	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"}
 	config.AllowHeaders = []string{"Origin", "Content-Type", "Accept", "Authorization"}
 
